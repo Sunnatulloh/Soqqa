4355baa20a482cc096867c191a2ed7e237d0f312
Sunny <38766651+Sunnatulloh@users.noreply.github.com>
2026-09-07 01:54:32 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
-import 'package:excel/excel.dart';
+import 'package:excel/excel.dart' hide Border;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
